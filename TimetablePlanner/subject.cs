81e4785017855f896ee0dc883b353651678df7b5
wektor-ops <wiktor_wisniewski@sluz.ch>
2025-10-23 11:32:38 +0200

    Klassen hinzufügen

---
+++ TimetablePlanner/subject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetablePlanner
+{
+    internal class subject
+    {
+
+    }
+}
