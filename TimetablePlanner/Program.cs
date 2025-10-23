a8b214087b29a126b595574db733fe8e2ee8d565
wektor-ops <wiktor_wisniewski@sluz.ch>
2025-10-23 11:16:17 +0200

    erstellung VS datei

---
+++ TimetablePlanner/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetablePlanner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
