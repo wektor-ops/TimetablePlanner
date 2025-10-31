3ff56ff0d3bf69a686b419fac2c0d13fe122e09a
wektor-ops <wiktor_wisniewski@sluz.ch>
2025-10-31 09:49:23 +0100

    Teacher Klasse implementiert

--- TimetablePlanner/student.cs
+++ TimetablePlanner/student.cs
@@ -9,12 +9,12 @@ namespace TimetablePlanner
     internal class Student
     {
         int _studentId;
-        string _name;
+        string _firstname;
         string _lastname;
         public Schoolclass _class;
         public List<Subject> _additionalSubject;
         public int StudentId { get { return _studentId; } }
-        public string Name { get { return _name; } }
+        public string Name { get { return _firstname; } }
         public string Lastname { get { return _lastname; } }
 
        
--- TimetablePlanner/teacher.cs
+++ TimetablePlanner/teacher.cs
@@ -8,5 +8,24 @@ namespace TimetablePlanner
 {
     internal class Teacher
     {
+        int _teacherId;
+        static int Iddistributor = 1;
+        string _abbreviation;
+        string _firstname;
+        string _lastName;
+
+        public int TeacherId { get { return _teacherId; }}
+        public string Abbrevation { get { return _abbreviation; }}
+        public string Firstname { get { return _firstname; } set { _firstname = value; } }
+        public string LastName { get { return _lastName; } set { _lastName = value; } }
+
+        public Teacher(string newFirstname, string newLastname)
+        {
+            _teacherId = Iddistributor++;
+            Firstname = newFirstname;
+            LastName = newLastname;
+            _abbreviation = newLastname.Substring(0, 2).ToUpper() + newFirstname.Substring(0, 1).ToUpper();
+        }
+        
     }
 }
