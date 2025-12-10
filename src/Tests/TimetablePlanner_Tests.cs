d4352c1dc723aa94f9f35c362b56ae5a13283552
wektor-ops <wiktor_wisniewski@sluz.ch>
2025-12-10 22:51:00 +0100

    änderung Projekt von .Net4.8 zu .Net8.0

---
+++ src/Tests/TimetablePlanner_Tests.cs
@@ -0,0 +1,39 @@
+﻿using TimetablePlanner;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class TimetablePlanner_Tests
+    {
+        [TestMethod]
+        public void Constructor_RoomPlan_IsInitializedWithCorrectDimensions()
+        {
+            // Arrange
+            int expectedDays = Timetable.Days;
+            int expectedHours = Timetable.Hours;
+
+            // Act
+            Room room = new Room("Test");
+
+            // Assert
+            Assert.IsNotNull(room.RoomPlan, "RoomPlan should be initialised");
+            Assert.AreEqual(expectedDays, room.RoomPlan.GetLength(0), "first dimension Day is wrong.");
+            Assert.AreEqual(expectedHours, room.RoomPlan.GetLength(1), "second dimension hour ist wrong");
+        }
+        [TestMethod]
+        public void Constructor_LongAbbreviation_LengthIsExactlyFour()
+        {
+            // Arrange
+            string inputAbbreviation = "Roomtolong"; 
+            int expectedLength = 4;
+            string expectedAbbreviation = "Room"; 
+
+            // Act
+            Room room = new Room(inputAbbreviation);
+
+
+            Assert.AreEqual(expectedLength, room.Abbreviation.Length,
+                "the abbrevation schould be four.");
+        }
+    }
+}
